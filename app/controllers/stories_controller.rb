c2413157dc8d87fa6d6cf3904c6e64f445b28805
wukrit <sukritwalia@gmail.com>
2019-09-18 09:57:24 -0400

    updated sort time

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,11 +2,6 @@ class StoriesController < ApplicationController
 
   def index
     @stories = Story.sort_by_newest
-    # respond_to do |format|
-    #   format.json do
-    #     render json: @stories.to_json
-    #   end
-    # end
     render :json => @stories
   end
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ApplicationRecord
 
   def self.sort_by_newest
-    self.all.sort_by{|story| Time.at(story.time)}.reverse
+    self.all.sort_by{|story| Time.at(story.time.to_i)}.reverse
   end
 end
